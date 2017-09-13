50a124a8e618849b5620224b9380915dd33ec276
Fred <domaine75@gmail.com>
2017-09-13 22:24:44 +0200

    Add another project to create a solution

---
+++ CreateSolution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CreateSolution
+{
+  class Program
+  {
+    static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+      string solution1 = string.Empty;
+      string[] array1 = new string[250];
+      for (int i = 1; i < 251; i++)
+      {
+        solution1 += i;
+        array1[i] = i.ToString();
+      }
+
+      
+      display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
