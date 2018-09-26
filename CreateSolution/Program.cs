76f742bed2abe95b226fd354bccca32fe2391744
fred <domaine75@gmail.com>
2018-09-27 00:12:26 +0200

    Add project to search for solution and refactor code

--- CreateSolution/Program.cs
+++ CreateSolution/Program.cs
@@ -2,19 +2,20 @@
 
 namespace CreateSolution
 {
-  class Program
+  internal class Program
   {
-    static void Main()
+    private static void Main()
     {
       Action<string> display = Console.WriteLine;
       string solution1 = string.Empty;
-      string[] array1 = new string[250];
+      var array1 = new string[251];
       for (int i = 1; i < 251; i++)
       {
         solution1 += i;
         array1[i] = i.ToString();
       }
 
+      display($"La chaine créée est {solution1}");
       
       display("Press any key to exit:");
       Console.ReadKey();
